03d5f7f8d15c4022a2cb86bec60c8edfe9d05e5c
BrandonSGC <brangmz20@gmail.com>
2023-06-18 23:03:45 -0600

    Finalizando Proyecto.

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,85 @@
-// Importamos express.
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const app = express();
 
-// Constante del Puerto a utilizar.
-const PUERTO = 3000;
+// Funciones de las DB.
+const {
+  insertarClienteSQLServer,
+  obtenerClientesSQLServer,
+} = require("./databaseSQLServer.js");
+const { insertarClienteMySQL } = require("./databaseMySQL.js");
 
-// Funciones de Insertar/Obtener datos de SQL Server.
-const { insertarClienteSQLServer } = require('./databaseSQLServer.js');
-const { obtenerClientesSQLServer } = require('./databaseSQLServer.js');
+const PUERTO = 3800;
 
-// Funciones de Insertar/Obtener datos de MySQL.
-const { insertarClienteMySQL } = require('./databaseMySQL.js');
+// Server.
+app.listen(PUERTO, () => {
+  console.log(`El servidor está escuchando en el puerto ${PUERTO}...`);
+});
 
-
-
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// Hacemos escuchar al servidor.
-app.listen(PUERTO, () => {
-    console.log(`El servidor está escuchando en el puerto ${PUERTO}...`);
-});
-
 // Configurar la entrega de archivos estáticos.
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
+// Routing.
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/html/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/html/index.html"));
 });
 
+app.post("/crearCliente", async (req, res) => {
+  try {
+    // Accedemos a los datos enviados del formulario.
+    const cedula = req.body.cedula;
+    const nombre = req.body.nombre;
+    const primerApellido = req.body.primerApellido;
+    const segundoApellido = req.body.segundoApellido;
+    const fechaString = req.body.fechaNacimiento;
+    const [year, month, day] = fechaString.split("-");
+    const fecha = new Date(year, month - 1, day);
+    const telefono = req.body.telefono;
+    const email = req.body.email;
+    const sexo = req.body.sexo;
+    const estado = req.body.estado === "0" ? false : true;
 
-// Insertar datos del cliente
-app.post('/crearCliente', async (req, res) => {
-    try {
-        // Accedemos a los datos enviados del formulario.
-        const cedula = req.body.cedula
-        const nombre = req.body.nombre
-        const primerApellido = req.body.primerApellido
-        const segundoApellido = req.body.segundoApellido        
-        const fechaString = req.body.fechaNacimiento;
-        const [year, month, day] = fechaString.split("-")
-        const fecha = new Date(year, month - 1, day);
-        const telefono = req.body.telefono;    
-        const email = req.body.email;
-        const sexo = req.body.sexo;
-        const estado = req.body.estado === "0" ? false : true;
+    // Insertar datos en DBs.
+    await insertarClienteSQLServer(
+      cedula,
+      nombre,
+      primerApellido,
+      segundoApellido,
+      fecha,
+      telefono,
+      email,
+      sexo,
+      estado
+    );
+    await insertarClienteMySQL(
+      cedula,
+      nombre,
+      primerApellido,
+      segundoApellido,
+      fecha,
+      telefono,
+      email,
+      sexo,
+      estado
+    );
 
-        // Insertar datos en SQL Server.
-        await insertarClienteSQLServer(cedula, nombre, primerApellido, segundoApellido, fecha, telefono, email, sexo, estado);        
-        // Insertar datos en MySQL.
-        await insertarClienteMySQL(cedula, nombre, primerApellido, segundoApellido, fecha, telefono, email, sexo, estado);
-        
-        res.send(`Datos guardados exitósamente.`);
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error al guardar los datos.');
-    }
+    res.send(`Datos guardados exitósamente.`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al guardar los datos.");
+  }
 });
 
-app.get('/obtenerClientes', async (req, res) => {  
-    try {
-        // Obtenemos los datos de los Clientes.
-        const clientes = await obtenerClientesSQLServer();
-
-
-        
-        
-        // Enviamos el array de objetos en JSON.
-        res.json(clientes);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Error al obtener los datos del cliente.');
-    }
+app.get("/obtenerClientes", async (req, res) => {
+  try {
+    const clientes = await obtenerClientesSQLServer();
+    res.json(clientes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error al obtener los datos del cliente.");
+  }
 });
